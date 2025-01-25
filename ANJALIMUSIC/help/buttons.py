0eafc6ddd6049be8ec050b9774fbcae8b47755f0
VENOM <venommusic91@gmail.com>
2025-01-25 14:22:45 +0530

    Update buttons.py

--- ANJALIMUSIC/help/buttons.py
+++ ANJALIMUSIC/help/buttons.py
@@ -11,21 +11,25 @@ class BUTTONS(object):
         ],
         [
             InlineKeyboardButton("sєᴧʀᴄʜ", callback_data="TOOL_BACK HELP_02"),
-            InlineKeyboardButton("ᴛᴛs", callback_data="TOOL_BACK HELP_03"),
-            InlineKeyboardButton("ɪηғσ", callback_data="TOOL_BACK HELP_04"),
+            InlineKeyboardButton("ᴛᴛs", callback_data="TOOL_BACK HELP_03"),            
         ],
         [
-            InlineKeyboardButton("ғσηᴛ", callback_data="TOOL_BACK HELP_05"),
+            InlineKeyboardButton("ɪηғσ", callback_data="TOOL_BACK HELP_04"),
+            InlineKeyboardButton("ғσηᴛ", callback_data="TOOL_BACK HELP_05"),            
+        ],
+        [
             InlineKeyboardButton("ϻᴧᴛʜ", callback_data="TOOL_BACK HELP_06"),
             InlineKeyboardButton("ᴛᴧɢᴧʟʟ", callback_data="TOOL_BACK HELP_07"),
         ],
         [
             InlineKeyboardButton("ɪϻᴧɢє", callback_data="TOOL_BACK HELP_08"),
-            InlineKeyboardButton("ʜᴧsᴛᴧɢ", callback_data="TOOL_BACK HELP_09"),
-            InlineKeyboardButton("sᴛɪᴄᴋєʀs", callback_data="TOOL_BACK HELP_10"),
+            InlineKeyboardButton("ʜᴧsᴛᴧɢ", callback_data="TOOL_BACK HELP_09"),            
         ],
         [
-            InlineKeyboardButton("ғυη", callback_data="TOOL_BACK HELP_11"),
+            InlineKeyboardButton("sᴛɪᴄᴋєʀs", callback_data="TOOL_BACK HELP_10"),
+            InlineKeyboardButton("ғυη", callback_data="TOOL_BACK HELP_11"),            
+        ],
+        [
             InlineKeyboardButton("ǫυσᴛʟʏ", callback_data="TOOL_BACK HELP_12"),
             InlineKeyboardButton("ᴛ-ᴅ", callback_data="TOOL_BACK HELP_13"),
         ],
@@ -39,21 +43,25 @@ class BUTTONS(object):
         ],
         [
             InlineKeyboardButton("ʙᴧη", callback_data="MANAGEMENT_BACK HELP_14"),
-            InlineKeyboardButton("ᴋɪᴄᴋ", callback_data="MANAGEMENT_BACK HELP_15"),
-            InlineKeyboardButton("ϻυᴛє", callback_data="MANAGEMENT_BACK HELP_16"),
+            InlineKeyboardButton("ᴋɪᴄᴋ", callback_data="MANAGEMENT_BACK HELP_15"),            
         ],
         [
-            InlineKeyboardButton("ᴘɪη", callback_data="MANAGEMENT_BACK HELP_17"),
+            InlineKeyboardButton("ϻυᴛє", callback_data="MANAGEMENT_BACK HELP_16"),
+            InlineKeyboardButton("ᴘɪη", callback_data="MANAGEMENT_BACK HELP_17"),            
+        ],
+        [
             InlineKeyboardButton("sᴛᴧғғ", callback_data="MANAGEMENT_BACK HELP_18"),
             InlineKeyboardButton("sєᴛ-υᴘ", callback_data="MANAGEMENT_BACK HELP_19"),
         ],
         [
             InlineKeyboardButton("ᴢσϻʙɪє", callback_data="MANAGEMENT_BACK HELP_20"),
-            InlineKeyboardButton("ɢᴧϻє", callback_data="MANAGEMENT_BACK HELP_21"),
-            InlineKeyboardButton("ɪϻᴘσsᴛєʀ", callback_data="MANAGEMENT_BACK HELP_22"),
+            InlineKeyboardButton("ɢᴧϻє", callback_data="MANAGEMENT_BACK HELP_21"),            
         ],
         [
-            InlineKeyboardButton("sɢ", callback_data="MANAGEMENT_BACK HELP_23"),
+            InlineKeyboardButton("ɪϻᴘσsᴛєʀ", callback_data="MANAGEMENT_BACK HELP_22"),
+            InlineKeyboardButton("sɢ", callback_data="MANAGEMENT_BACK HELP_23"),         
+        ],
+        [
             InlineKeyboardButton("ᴛʀ", callback_data="MANAGEMENT_BACK HELP_24"),
             InlineKeyboardButton("ɢʀᴧᴘʜ", callback_data="MANAGEMENT_BACK HELP_26"),
         ],
