e551056d5ff5f35c38a5791fd33c9364a3a7d57d
VENOM <venommusic91@gmail.com>
2025-01-25 16:21:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,35 +49,18 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = [
-    "https://files.catbox.moe/u7iwi5.jpg",
-    "https://files.catbox.moe/rkskww.jpg",
-    "https://files.catbox.moe/uz810n.jpg",
-    "https://files.catbox.moe/vzl4xb.jpg",
-    "https://files.catbox.moe/j2zdsu.jpg",
-    "https://files.catbox.moe/nlweex.jpg",
-    "https://files.catbox.moe/pp4god.jpg",
-    "https://files.catbox.moe/jslln0.jpg",
-    "https://files.catbox.moe/fqmdff.jpg",
-    "https://files.catbox.moe/eushjw.jpg",
-    "https://files.catbox.moe/39fg3k.jpg",
-]
-
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/4y6acl.jpg"
-)
-PLAYLIST_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/jrupn9.jpgg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/jrupn9.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/jrupn9.jpg"
-
-
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg")
+PLAYLIST_IMG_URL = "https://telegra.ph/file/79547e01862628bb85df0.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/79547e01862628bb85df0.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
