46057051ac8e01fb78bdc101482c9526ec7e5895
VENOM <venommusic91@gmail.com>
2025-01-25 11:20:27 +0530

    Update write.py

--- ANJALIMUSIC/plugins/Yumi/write.py
+++ ANJALIMUSIC/plugins/Yumi/write.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from config import  BOT_USERNAME
-from SACHIN_MUSIC import app as app
+from ANJALIMUSIC import app as app
 import requests
 
 @app.on_message(filters.command("write"))
